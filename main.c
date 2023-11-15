1f059e74019aa6c093dc5d2ffab31e53b775afe6
ranaali3 <ranaronaali@gmail.com>
2023-11-15 17:24:29 +0200

    create and test multiply function

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 // this is the main code
 
 #include <stdio.h>
-
-
+float mul(){
+    float num1,num2;
+    float mulResult;
+printf("enter number 1:");
+    scanf("%f",&num1);
+    printf("enter number 2:");
+    scanf("%f",&num2);
+    mulResult=num1*num2;
+    printf("result=%f\n",mulResult);
+    return mulResult;
+}
 int main()
 {
+
+
+    float mul_result=mul();
+
+
     return 0;
 }
