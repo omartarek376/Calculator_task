d0e7bc199b711e4c7ac8226a53d7bd798a525087
Shirehan <shirehanmedhat@gmail.com>
2023-11-17 21:05:12 +0200

    Sub Function

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 // this is the main code
-
+float sub_fun (float in1 , float in2);
 #include <stdio.h>
 
 
 int main()
 {
     return 0;
+}
+
+float sub_fun (float in1 , float in2)
+{
+
+	return (in1-in2);
 }
