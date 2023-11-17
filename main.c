14be11021ecdf2441eed5b87d85b41d14f4b9cf0
unknown <farahmohey22@gmail.com>
2023-11-17 21:06:37 +0200

    add function

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
-// this is the main code
-
-#include <stdio.h>
+#include<stdio.h>
 
 
-int main()
-{
-    return 0;
+float add (float x,float y){
+	float sum;
+	sum= x+y;
+	return sum;
 }
+
+int main(){
+	//setvbuf(stdout, NULL, _IONBF, 0);  
+	//setvbuf(stderr, NULL, _IONBF, 0);
+
+	float a;
+	float b;
+	float result;
+
+	printf("Please enter 2 numbers to get the sum:\n");
+	scanf("%f %f",&a,&b);
+
+	result = add(a,b);
+	printf("Result of sum = %f", result);
+
+	return 0;
+}
+
